700a5ba31bec21e2bf57a687ff840c87c8189587
indiakka <78439130+indiakka@user.noreply.github.com>
2024-04-16 16:44:54 +0200

    cambiar rutas de imágenes Co-authored-by: Ricard Mallafre <rikitzzz@gmail.com>

--- src/pages/Proyectos/Proyectos.jsx
+++ src/pages/Proyectos/Proyectos.jsx
@@ -4,45 +4,45 @@ import "./proyectos.css";
 const Proyectos = () => {
   return (
     <div className="contenedor-proyectos">
-      <div className='proyectos' id="adoptaAmor">
+      <div className="proyectos" id="adoptaAmor">
         <h1>Adopta Amor</h1>
-        <NavLink to="">
-          <img src="src\assets\AdoptaAmor.png" />
+        <NavLink to="https://adopta-amor-f5.netlify.app/">
+          <img src="/assets/AdoptaAmor.png" />
         </NavLink>
       </div>
-      <div className='proyectos' id="workWave">
+      <div className="proyectos" id="workWave">
         <h1>WorkWave</h1>
         <NavLink to="">
-          <img src="src\assets\workWave.png" />
+          <img src="/assets/workWave.png" />
         </NavLink>
       </div>
-      <div className='proyectos' id="blogDeCafe">
+      <div className="proyectos" id="blogDeCafe">
         <h1>Blog de Café</h1>
         <NavLink to="https://blog-de-cafe-curso-desarrollo-web.netlify.app/">
-          <img src="src\assets\blogDeCafe.png" />
+          <img src="/assets/BlogDeCafe.png" />
         </NavLink>
       </div>
-      <div className='proyectos' id="freelancePage">
+      <div className="proyectos" id="freelancePage">
         <h1> Página Freelance</h1>
         <NavLink to="https://freelance-page-curso-desarrollo-web.netlify.app/">
-          <img src="src\assets\freelancePage.png" />
+          <img src="/assets/freelancePage.png" />
         </NavLink>
       </div>
-      <div className='proyectos' id="frontendStore">
+      <div className="proyectos" id="frontendStore">
         <h1>Tienda frontend</h1>
         <NavLink to="https://frontend-store-curso-desarrollo-web.netlify.app/">
-          <img src="src\assets\frontendStore.png" />
+          <img src="/assets/frontendStore.png" />
         </NavLink>
       </div>
-      <div className='proyectos' id="validacionDNI">
+      <div className="proyectos" id="validacionDNI">
         <h1>Validación DNI</h1>
-        <NavLink to="">
-          <img src="src\assets\ValidacionDNI.png" />
+        <NavLink to="https://verificacion-dni.netlify.app/">
+          <img src="/assets/ValidacionDNI.png" />
         </NavLink>
       </div>
-      <div className='proyectos' id="toquenElDom">
+      <div className="proyectos" id="toquenElDom">
         <h1>Fuga sonora</h1>
-        <img src="src\assets\toquenElDom.png" />
+        <img src="/assets/toquenElDom.png" />
         <NavLink to="https://toquen-el-dom-factoriaf5.netlify.app/"></NavLink>
       </div>
     </div>
