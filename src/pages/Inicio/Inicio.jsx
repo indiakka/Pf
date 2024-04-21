25eb60fcf73a285f49b892da11801e83c154fe10
indiakka <78439130+indiakka@user.noreply.github.com>
2024-04-21 17:52:40 +0200

    inicio media queries

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -1,19 +1,13 @@
-// import Background from "../../components/background/Background";
 import "./inicio.css";
 
 const Inicio = () => {
   return (
     <div className="inicio-container">
-      <div className="inicio">
-        {/* <Background /> */}
-        <div className="inicio-text">
-          <h1> ¡Hola! Soy Ziortza Rey</h1>
-          <p> Desarrolladora Web Junior</p>
-        </div>
-        <div className="inicio-image">
-          <img src="/assets/fotozrl.png" />
-        </div>
+      <div className="inicio-text">
+        <h1> ¡Hola! Soy Ziortza Rey</h1>
+        <p> Desarrolladora Web Junior</p>
       </div>
+        <img className="image-portfolio" src="/assets/fotozrl.png" />
     </div>
   );
 };
